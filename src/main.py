70d1cfef3e16be8264cac638b3d67f1302699582
DORENE VIVIAN AKIRU <doreneakiru@gmail.com>
2026-08-30 16:50:13 +0300

    Add greet function to utils.py and integrate personalized greeting in main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-from utils import square, is_even, celsius_to_fahrenheit
+from utils import square, is_even, celsius_to_fahrenheit, greet
+
+name = input("Enter your name: ")
+print(greet(name))
 
 number = float(input("Enter a number: "))
-
 print(f"Square: {square(number)}")
-
 if is_even(number):
     print(f"{number} is even")
 else:
     print(f"{number} is odd")
-
 print(f"Fahrenheit equivalent: {celsius_to_fahrenheit(number)}")
-
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+
+
 def square(n):
     return n ** 2
 
@@ -9,3 +11,6 @@ def is_even(n):
 def celsius_to_fahrenheit(c):
     return (c * 9/5) + 32
 
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Python Lab."
